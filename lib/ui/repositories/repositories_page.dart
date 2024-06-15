9d164846773938ce6c7a052d2fa6bf4ce0ef1eea
ShunMc <news_two_three@yahoo.co.jp>
2024-06-16 02:00:38 +0900

    命名変更

--- lib/ui/repositories/repositories_page.dart
+++ lib/ui/repositories/repositories_page.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_graphql_study/gen/graphql/search.graphql.dart';
+import 'package:flutter_graphql_study/gen/graphql/repositories.graphql.dart';
 import 'package:flutter_graphql_study/utility/extensions/query_hook_result.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
